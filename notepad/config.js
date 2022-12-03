179e6b4501a13017ea5885eee3bd3b475d06a242
Thiệu Trung Kiên <87187870+TrunqKj3n@users.noreply.github.com>
2022-12-03 17:38:52 +0700

    Update config.js

--- notepad/config.js
+++ notepad/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: '', // URL MongoDB atlat o day
+    mongoURI: 'mongodb+srv://ttktrungkien:trungkien@cluster0.0rs9x.mongodb.net/?retryWrites=true&w=majority', // URL MongoDB atlat o day
     port : 8080
 }
